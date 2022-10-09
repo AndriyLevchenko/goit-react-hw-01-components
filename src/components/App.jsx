7c4761a8522a987453731df37d054d2fcee9c05b
AndriyLevchenko <98983557+AndriyLevchenko@users.noreply.github.com>
2022-10-09 17:53:06 +0300

    React частина (рендер)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import {Profile} from 'components/Profile/Profile';
+import user from 'components/user.json';
+import {Statistics} from 'components/Statistics/Statistics';
+import data from 'components/data.json';
+import { FriendList } from './FriendList/FriendList';
+import friends from 'components/friends.json';
+import {TransactionHistory} from './TransactionHistory/TransactionHistory';
+import transactions from 'components/transactions.json'
+
 export const App = () => {
+  const { username, tag, location, avatar, stats } = user;
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+      <Statistics title='Upload stats' stats={data}/>
+      <FriendList friends={friends}/>
+      <TransactionHistory items={transactions}/>
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,11 @@
+import { FriendListItem } from "./FriendListItem";
+
+export const FriendList = ({friends}) => {
+    return (
+    <ul class="friend-list">
+  {friends.map(friend => (
+    <FriendListItem key={friend.id} avatar={friend.avatar} name={friend.name} isOnline={friend.isOnline}/>
+  ))}
+    </ul>
+    )
+}

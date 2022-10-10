9420b83cfd7ad3fb3694af4982bf3f9eec9a6b30
AndriyLevchenko <98983557+AndriyLevchenko@users.noreply.github.com>
2022-10-10 22:57:48 +0300

    HW React №1
    Відкориговане дз

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -3,7 +3,8 @@ import css from 'components/FriendList/FriendListItem.module.css'
 
 export const FriendListItem = ({avatar, name, isOnline}) => {
     return (<li className={css.item}>
-    {isOnline ? (<span className={`${css.status} ${css.onLine}`}></span>):(<span className={`${css.status} ${css.ofLine}`}></span>)}
+    {isOnline ? (<span className={`${css.onLine}`}></span>):
+    (<span className={`${css.ofLine}`}></span>)}
     <img className={css.avatar} src={avatar} alt={name} width="48" />
     <p className={css.name}>{name}</p>
   </li>)
